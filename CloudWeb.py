2d0e98b8a0f5fcb1a019bca87a9f862d84c6bc1f
Cloudcorpoffical <157359451+Cloudcorpoffical@users.noreply.github.com>
2024-01-22 12:25:38 +0300

    Add files via upload

---
+++ CloudWeb.py
@@ -0,0 +1,119 @@
+from PyQt6.QtCore import *
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import *
+from PyQt6.QtWebEngineWidgets import *
+from PyQt6.QtPrintSupport import *
+import os
+import sys
+ 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+class MainWindow(QMainWindow):
+ 
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.tabs = QTabWidget()
+        self.tabs.setDocumentMode(True)
+        self.tabs.tabBarDoubleClicked.connect(self.tab_open_doubleclick)
+        self.tabs.currentChanged.connect(self.current_tab_changed)
+        self.tabs.setTabsClosable(True)
+        self.tabs.tabCloseRequested.connect(self.close_current_tab)
+        self.setCentralWidget(self.tabs)
+        self.status = QStatusBar()
+        self.setStatusBar(self.status)
+        navtb = QToolBar("Navigation")
+        self.addToolBar(navtb)
+        back_btn_icon = (dir_path + r'\back.png')
+        back_btn = QAction("Back", self)
+        back_btn.setIcon(QIcon(back_btn_icon))
+        back_btn.setStatusTip("Back to previous page")
+        back_btn.triggered.connect(lambda: self.tabs.currentWidget().back())
+        navtb.addAction(back_btn)
+        forward_btn_icon = (dir_path + r'\forward.png')
+        next_btn = QAction("Forward", self)
+        next_btn.setIcon(QIcon(forward_btn_icon))
+        next_btn.setStatusTip("Forward to next page")
+        next_btn.triggered.connect(lambda: self.tabs.currentWidget().forward())
+        navtb.addAction(next_btn)
+        reload_btn_icon = (dir_path + r'\reload.png')
+        reload_btn = QAction("Reload", self)
+        reload_btn.setIcon(QIcon(reload_btn_icon))
+        reload_btn.setStatusTip("Reload page")
+        reload_btn.triggered.connect(lambda: self.tabs.currentWidget().reload())
+        navtb.addAction(reload_btn)
+        home_btn_icon = (dir_path + r'\home.png')
+        home_btn = QAction("Home", self)
+        home_btn.setIcon(QIcon(home_btn_icon))
+        home_btn.setStatusTip("Go home")
+        home_btn.triggered.connect(self.navigate_home)
+        navtb.addAction(home_btn)
+        navtb.addSeparator()
+        self.urlbar = QLineEdit()
+        self.urlbar.returnPressed.connect(self.navigate_to_url)
+        navtb.addWidget(self.urlbar)
+        stop_btn_icon = (dir_path + r'\stop.png')
+        stop_btn = QAction("Stop", self)
+        stop_btn.setIcon(QIcon(stop_btn_icon))
+        stop_btn.setStatusTip("Stop loading current page")
+        stop_btn.triggered.connect(lambda: self.tabs.currentWidget().stop())
+        navtb.addAction(stop_btn)
+        self.add_new_tab(QUrl('http://google.com'), 'Homepage')
+        self.show()
+        self.setWindowTitle("CloudWeb")
+ 
+    
+    def add_new_tab(self, qurl = None, label ="Blank"):
+ 
+        if qurl is None:
+            qurl = QUrl('http://google.com')
+
+        browser = QWebEngineView()
+        browser.setUrl(qurl)
+        i = self.tabs.addTab(browser, label)
+        self.tabs.setCurrentIndex(i)
+        browser.urlChanged.connect(lambda qurl, browser = browser:
+                                   self.update_urlbar(qurl, browser))
+        browser.loadFinished.connect(lambda _, i = i, browser = browser:
+                                     self.tabs.setTabText(i, browser.page().title()))
+
+    def tab_open_doubleclick(self, i):
+        if i == -1:
+            self.add_new_tab(qurl=QUrl("http://dzen.ru"))
+
+    def current_tab_changed(self, i):
+        qurl = self.tabs.currentWidget().url()
+        self.update_urlbar(qurl, self.tabs.currentWidget())
+        self.update_title(self.tabs.currentWidget())
+
+    def close_current_tab(self, i):
+        if self.tabs.count() < 2:
+            return
+        self.tabs.removeTab(i)
+
+    def update_title(self, browser):
+        if browser != self.tabs.currentWidget():
+            return
+        title = self.tabs.currentWidget().page().title()
+        self.setWindowTitle("% s - CloudWeb" % title)
+
+    def navigate_home(self):
+        self.tabs.currentWidget().setUrl(QUrl("http://google.com"))
+
+    def navigate_to_url(self):
+        q = QUrl(self.urlbar.text())
+        if q.scheme() == "":
+            q.setScheme("http")
+        self.tabs.currentWidget().setUrl(q)
+
+    def update_urlbar(self, q, browser = None):
+        if browser != self.tabs.currentWidget():
+            return
+        self.urlbar.setText(q.toString())
+        self.urlbar.setCursorPosition(0)
+
+app = QApplication(sys.argv)
+app.setApplicationName("Geek PyQt5")
+window = MainWindow()
+app.exec()
